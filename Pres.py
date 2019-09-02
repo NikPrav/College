2aeb7bd9eda96e690e8fb6583ca40f66bb69c381
NikhilPraveen <54613572+NikhilPraveen@users.noreply.github.com>
2019-09-02 09:47:00 +0530

    Update Pres.py

--- Pres.py
+++ Pres.py
@@ -6,14 +6,16 @@ from coeffs import *
 #Plotting the circle
 len=100
 
-
-r=2.835533905
+x=np.array([9,7])
+r=2.828
 x=np.zeros((len))
 theta = np.linspace(0,2*np.pi,len)
 x_circ = np.zeros((2,len))
 x_circ[0,:] = r*np.cos(theta)+8
 x_circ[1,:] = r*np.sin(theta)+6
 plt.plot(x_circ[0,:],x_circ[1,:],label='$f(x)$')
+plt.plot(10,8,'o')
+plt.text(10,8,'X')
 
 #Plotting the line
 A = np.array([9,9])
@@ -35,6 +37,11 @@ plt.legend( loc='best')
 
 plt.xlim(-5, 15)
 plt.ylim(0, 22)
+
+
+#plt.savefig('../figs/2.4.eps')
+plt.show()
+
 
 
 #plt.savefig('../figs/2.4.eps')
